cbfe9d8a464fd847c22874f77c7364e8b7a21b72
poem <normamoras@gmail.com>
2020-07-15 14:44:17 -0400

    add first 3 components

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Index from './components/layout/Index';
+
+
+
+function App() {
+  return (
+   <Router>
+     <React.Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Index} />
+          </Switch>
+        </div>
+     </React.Fragment>
+   </Router>  
+  );
+}
+
+export default App;
---
+++ my-app/src/context.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+const Context = React.createContext();
+// export default class context extends Component
+//don't want to send class as default becuase we are exporting consumer
+export class Provider extends Component {
+    state = {
+        track_list: [
+
+        ],
+        heading: 'Top 10 Tracks'
+    }
+    render() {
+        return (
+           <Context.Provider value={this.state}>
+               {/* expression */}
+                {this.props.children}
+           </Context.Provider>
+        )
+    }
+}
+
+export const Consumer = Context.Consumer
