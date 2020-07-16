0a706fb3bbbc902a24834cf5d42e79d098235f07
poem <normamoras@gmail.com>
2020-07-16 16:59:43 -0400

    added search componenet

--- my-app/src/components/tracks/Search.js
+++ my-app/src/components/tracks/Search.js
@@ -4,13 +4,30 @@ import { Context } from '../../context';
 const Search = () => {
     const[state, setState] = useContext(Context)
     const[userInput, setUserInput] = useState( '' ) 
+    const[ trackName, setTrackName] = useState('')
+
+    const findTrack = e => {
+        e.preventDefault();
+        setTrackName(userInput)
+    };
+    
     return (
         <div >
             <form>
                 <div className="input-group">
-                    <input type="text" class="form-control" placeholder="Search Lyrics"/>
+                    <input 
+                        type="text" 
+                        class="form-control" 
+                        placeholder="Song Title..."
+                        onChange={ e => setUserInput(e.target.value)}
+                    />
                     <div className="input-group-append">
-                        <button className="btn btn-outline-primary" type="button">Search</button>
+                        <button 
+                            className="btn btn-outline-primary" 
+                            type="button"
+                            onClick = {findTrack}>
+                            Search
+                        </button>
                     </div>
                 </div>
             </form>  
