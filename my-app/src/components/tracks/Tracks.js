00228318a7b8409c7b084e6b21034cbe6dcba767
poem <normamoras@gmail.com>
2020-07-16 07:11:41 -0400

    adding contextcontroller to app.js

--- my-app/src/components/tracks/Tracks.js
+++ my-app/src/components/tracks/Tracks.js
@@ -1,35 +1,6 @@
 import React, { Component } from 'react'
-import { Consumer } from '../../context';
+import { Context } from '../../context';
 import Track from './Track';
 import Spinner from '../layout/Spinner';
 
 
-class Tracks extends Component {
-    render() {
-        return (
-          <Consumer>
-              {value => {
-                  const { track_list } = value;
-
-                  if(track_list === undefined || track_list.length === 0){
-                    return <Spinner />
-                } else {
-                    return (
-                        <React.Fragment>
-                            {/* <h3 className="text-center mb-4">{heading}</h3> */}
-                            <div className="row">
-                                {track_list.map(item => (
-                                    <Track key={item.track.track_id} track={item.track}/>
-                                ))}
-                            </div>
-                        </React.Fragment>
-                    );
-                }
-              }}
-              <h1>Tracks</h1>
-          </Consumer>
-        )
-    }
-}
-
-export default Tracks;
--- my-app/src/context.js
+++ my-app/src/context.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Context = React.createContext();
+export const Context = React.createContext();
 // export default class context extends Component
 //don't want to send class as default becuase we are exporting consumer
-export class Provider extends Component {
-    state = {
-        track_list: [
 
-        ],
-        heading: 'Top 10 Tracks'
-    }
+export function ContextController({ children }){
+    let intialState = {
+        track_list: [],
+        heading: ''
+    };
 
-    componentDidMount() {
-        axios.get(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&
-        page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
-            .then( res=> {
-                console.log(res.data);
-                this.setState({track_list: res.data.message.body});
-            })
-            .catch( err => console.log(err));
-    }
-    render() {
-        return (
-           <Context.Provider value={this.state}>
-               {/* expression */}
-                {this.props.children}
-           </Context.Provider>
-        )
-    }
+const [state, setState] = useState(initialState)
+
+useEffect( () => {
+    axios
+        .get(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&
+        page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY
+        }`
+    )
+        .then( res=> {
+        console.log(res.data);
+        setState({
+            track_list: res.data.message.body.track_list,
+            heading: "Top 10 Tracks"
+        });
+    })
+    .catch( err => console.log(err));  
+}, []);
+
+return (
+    <Context.Provider value={[state, setState]}>{children}</Context.Provider>
+);
+
 }
-
-export const Consumer = Context.Consumer
