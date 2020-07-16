de6282952051e6b951a6d7cb52a0ef9ba4e44fb2
poem <normamoras@gmail.com>
2020-07-16 06:52:26 -0400

    adding contextcontroller to app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,23 +3,23 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Index from './components/layout/Index';
-import { Provider } from './context';
+import { ContextController } from './context';
 
 
 function App() {
   return (
-  <Provider>
+  <ContextController>
    <Router>
-     <React.Fragment>
+     <>
         <Navbar />
         <div className="container">
           
             <Route exact path="/" component={Index} />
          
         </div>
-     </React.Fragment>
+     </>
    </Router> 
-   </Provider>  
+   </ContextController>  
   );
 }
 
